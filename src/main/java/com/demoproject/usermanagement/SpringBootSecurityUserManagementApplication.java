1470e8f2463a9761bf0af294708a279ae2a2f043
unknown <ahasanul.hadi@gmail.com>
2023-04-11 12:02:08 +0600

    Initial commit

---
+++ src/main/java/com/demoproject/usermanagement/SpringBootSecurityUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.demoproject.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoproject/usermanagement/SpringBootSecurityUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoproject.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
